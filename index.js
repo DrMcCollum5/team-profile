d0518c4c469dc54f4a11566b68c7aefb7d4150a9
DrMcCollum5 <94315015+DrMcCollum5@users.noreply.github.com>
2022-02-09 20:00:39 -0500

    updated index to read intern, and engineer cards

--- index.js
+++ index.js
@@ -2,82 +2,163 @@ const Manager = require("./Lib/Manager");
 const Intern = require("./Lib/Intern");
 const Engineer = require("./Lib/Engineer");
 const inquirer = require("inquirer");
+const path = require ("path");
 const fs = require("fs");
+const generateHtml = require ("./src/generatehtml");
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const distPath = path.join(DIST_DIR, 'allEmployees.html');
 
 var allEmployees = [];
 
 // start with a function asking for manager info
 const addManager = () => {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Who is the manager?"
-    //   validate: (answer) => {
-    //     if (answer) {
-    //       return true;
-    //     }
-    //     return "Please enter the manager's name!";
-    //   },
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "Please enter the manager's ID."
-    //   validate: (nameInput) => {
-    //     if (nameInput) {
-    //       console.log("Please enter the manager's ID!");
-    //       return false;
-    //     } else {
-    //       return true;
-    //     }
-    //   },
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Please enter the manager's email."
-    //   validate: (nameInput) => {
-    //     if (nameInput) {
-    //       return true;
-    //     } else {
-    //       console.log("Please enter an email!");
-    //       return false;
-    //     }
-    //   },
-    },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "Please enter the manager's office number"
-    //   validate: (nameInput) => {
-    //     if (nameInput) {
-    //       return true;
-    //     } else {
-    //       console.log("Please enter an office number!");
-    //     }
-    //     return false;
-    //   },
-    },
-  ]).then((answer) => {
-        console.log('Questions finsihed')
-        const manager = new Manager(
-            answer.name,
-            answer.id,
-            answer.email,
-            answer.officeNumber
-            );
-       
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Who is the manager?",
+        
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the manager's ID.",
+        
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the manager's email.",
+        
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter the manager's office number",
+        
+      },
+    ])
+    .then((answer) => {
+      console.log("Questions finsihed");
+      const manager = new Manager(
+        answer.name,
+        answer.id,
+        answer.email,
+        answer.officeNumber
+      );
 
+      allEmployees.push(manager);
 
-  allEmployees.push(manager)
-    
-
-    allEmployees.push(manager);
-    console.log(manager);
-    
-})
+      //    allEmployees.push(manager);
+      console.log(manager);
+      directory();
+    });
 };
+
+function directory() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "directoryChoice",
+        message: "What employee would you like to add next?",
+        choices: ["engineer", "intern", "im done adding employees"],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.directoryChoice) {
+        case "engineer":
+          addEngineer();
+          break;
+        case "intern":
+          addIntern();
+          break;
+        default:
+          buildHTML();
+      }
+    });
+}
+
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Who is the engineer?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the engineer's ID.",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the engineer's email.",
+      },
+      {
+        type: "input",
+        name: "gitHub",
+        message: "Please enter the engineer's gitHub.",
+      },
+    ])
+    .then((answer) => {
+      console.log("Questions finsihed");
+      const engineer = new Engineer(
+        answer.name,
+        answer.id,
+        answer.email,
+        answer.gitHub
+      );
+
+      allEmployees.push(engineer);
+
+      console.log(engineer);
+      directory();
+    });
+}
+
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Who is the intern?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the intern's ID.",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the intern's email.",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Please enter the intern's school",
+      },
+    ])
+    .then((answer) => {
+      console.log("Questions finsihed");
+      const intern = new Intern(
+        answer.name,
+        answer.id,
+        answer.email,
+        answer.school
+      );
+
+      allEmployees.push(intern);
+
+      console.log(intern);
+      directory();
+    });
+}
 
 const addEmployee = () => {
   console.log("");
@@ -175,7 +256,13 @@ const addEmployee = () => {
     });
 };
 
-function buildHTML() {}
+function buildHTML() {
+  if (!fs.existsSync(DIST_DIR)){
+    fs.mkdirSync(DIST_DIR);
+  }
+  fs.writeFileSync(distPath, generateHtml(allEmployees), 'utf-8');
+
+}
 const writeFile = () => {
   console.log("This is where I would build html");
 
